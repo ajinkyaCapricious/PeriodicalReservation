4be14e09f282c21b2b0a435614281eb328128ad1
ajinkyaCapricious <153263713+ajinkyaCapricious@users.noreply.github.com>
2023-12-08 12:40:54 +0530

    Initial setup
    Project created

---
+++ PeriodicalReservation/PeriodicalReservation/PeriodicalReservationApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeriodicalReservationApp.swift
+//  PeriodicalReservation
+//
+//  Created by Ajinkya Adole on 08/12/23.
+//
+
+import SwiftUI
+
+@main
+struct PeriodicalReservationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
